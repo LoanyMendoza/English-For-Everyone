20876e481404eccf0ed2e6f64146f80451e1171d
LoanyMendoza <suarezgabriela16@hotmail.com>
2024-01-16 23:42:47 -0600

    panel added to cellphone mode

--- code/main.js
+++ code/main.js
@@ -16,14 +16,76 @@ typewriter
 */
 
 
-$('.menuicon').click(function(e) {
-    e.preventDefault();
-    if($('navprincipal').hasClass('viewMenu'))
-    {
-        $('navprincipal').removeClass('viewMenu');
+// Es una locaaaa (baile setsi)
+// https://www.tiktok.com/@karkarchezito/video/7137660725006241029?lang=es
+
+// $('.menuicon').click(function(e) {
+//     e.preventDefault();
+//     if($('navprincipal').hasClass('viewMenu'))
+//     {
+//         $('navprincipal').removeClass('viewMenu');
+//     }else{
+//         $('navprincipal').addClass('viewMenu');
+//     }
+// });
+
+
+const icono = document.querySelector(".icono"); // Primero seleccionamos el elemento del icono
+// asignamos un evento de click al icono
+icono.addEventListener("click", function(evento){
+    evento.preventDefault(); // eliminamos el evento por default
+    // comprobamos si tiene 1 clase únicamente
+    // en caso de que sea solo 1 significa que el menú no está desplegado
+    
+    if(icono.classList.length == 1){ 
+        //Como no está desplegado generamos código HTML
+        mostrarMenu();
+        icono.classList.add("viewMenu");
     }else{
-        $('navprincipal').addClass('viewMenu');
+        borrarMenu();
+        icono.classList.remove("viewMenu");
     }
 });
 
+function borrarMenu(){
+    const header = document.querySelector(".header");
+    const menu = document.querySelector(".navprincipal");
+    header.removeChild(menu);
+}
 
+function mostrarMenu(){
+    const header = document.querySelector('.header');
+    const menu = document.createElement('NAV'); // Creamos el nav principal
+    const elemento1 = document.createElement('A'); // creamos los elementos del nav
+    const elemento2 = document.createElement('A'); // creamos los elementos del nav
+    const elemento3 = document.createElement('A'); // creamos los elementos del nav
+    const elemento4 = document.createElement('A'); // creamos los elementos del nav
+    
+    //agregamos clases a cada elemento
+    menu.classList.add("navprincipal")
+    elemento1.classList.add("navprincipal__elemento");
+    elemento1.classList.add("pagina-actual");
+    elemento2.classList.add("navprincipal__elemento");
+    elemento3.classList.add("navprincipal__elemento");
+    elemento4.classList.add("navprincipal__elemento");
+
+    // agregamos href a cada elemento
+    elemento1.href = 'index.html';
+    elemento2.href = 'facts.html';
+    elemento3.href = 'content.html';
+    elemento4.href = 'activities.html';
+
+    // Agregamos el contenido a cada elemento
+    elemento1.textContent = "Inicio";
+    elemento2.textContent = "Beneficios";
+    elemento3.textContent = "Contenidos";
+    elemento4.textContent = "Actividades";
+
+    // agregamos el elemento NAV a html en el header
+    header.appendChild(menu);
+    menu.appendChild(elemento1);
+    menu.appendChild(elemento2);
+    menu.appendChild(elemento3);
+    menu.appendChild(elemento4);
+
+}
